6a3aa243c09efde918431aa91a7db2a2b71cc9e2
ifeanyi <ifeanyiinnocentokoro@gmail.com>
2026-03-15 05:34:30 -0700

    k

--- lib/mail.ts
+++ lib/mail.ts
@@ -46,6 +46,8 @@ export interface LeadNotificationData {
 export async function sendLeadNotificationEmail(lead: LeadNotificationData) {
   const ADMIN_EMAILS = [
     "support@samish.com",
+    process.env.SMTP_EMAIL!,
+    
     // add more admin emails here if needed
   ];
 
